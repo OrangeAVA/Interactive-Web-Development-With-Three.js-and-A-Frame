cfa86e5d802086a03f4d3c01633bd5f966cfd87e
astraccia <astraccia@gmail.com>
2024-01-04 18:54:42 +0000

    Feat: chapter 8 code

--- chapter08/aframe_webVR_exhibition/js/components.js
+++ chapter08/aframe_webVR_exhibition/js/components.js
@@ -1,126 +1,108 @@
-AFRAME.registerComponent('nav-pointer', {
-  init: function () {
-    const el = this.el;
+AFRAME.registerComponent('scene-environment', {
 
-    // On click, send the NPC to the target location.
-    el.addEventListener('click', (e) => {
-      const ctrlEl = el.sceneEl.querySelector('[nav-agent]');
-      ctrlEl.setAttribute('nav-agent', {
-        active: true,
-        destination: e.detail.intersection.point
-      });
-    });
+    schema: {
+        enabled: {default: true},
+        hdrImage: {default: ''},
+        hdrExposure: {default: 1}
+    },
 
-    // When hovering on the nav mesh, show a green cursor.
-    el.addEventListener('mouseenter', () => {
-      el.setAttribute('material', {color: 'green'});
-    });
-    el.addEventListener('mouseleave', () => {
-      el.setAttribute('material', {color: 'crimson'})
-    });
+    init: function() {
 
-    // Refresh the raycaster after models load.
-    el.sceneEl.addEventListener('object3dset', () => {
-      this.el.components.raycaster.refreshObjects();
-    });
+      let enabled = this.data.enabled;
+      let hdrImage = this.data.hdrImage;
+      let hdrExposure = this.data.hdrExposure;
+      
+      if (!enabled) return;
+
+      const sceneEl = this.el;
+      const scene = sceneEl.object3D;
+      const renderer = sceneEl.renderer;
+
+      new THREE.RGBELoader()
+        .load( hdrImage, function ( texture ) {
+
+          texture.mapping = THREE.EquirectangularReflectionMapping;
+          
+          scene.background = texture;
+          scene.environment = texture;
+
+          renderer.toneMapping = THREE.LinearToneMapping;
+          renderer.toneMappingExposure = hdrExposure;
+
+          renderer.outputEncoding = THREE.sRGBEncoding;
+
+      } );
+    },
+    update: function() {
+    },
+    tick: function() {
+    }
+});
+
+AFRAME.registerComponent('art-pieces', {
+
+  schema: {
+      path: {default: ""},
+      customScale: {default: 2}
+  },
+
+  init: function() {
+
+    //console.log(this.data.path)
+    const el = this.el
+
+    fetch(this.data.path)
+      .then((response)=> response.json())
+      .then((json) => 
+      {
+        //console.log(json)
+        //console.log(el)
+       
+        Object.keys(json).map((pieceKey, index) =>{
+          
+          const curPiece = json[pieceKey] 
+          
+          el.innerHTML += `
+          <a-box 
+          material="src: ${curPiece.src}"
+          scale=" ${curPiece.dimensions[1] * curPiece.customScale} ${curPiece.dimensions[0] * curPiece.customScale} 0.08"
+          position="${curPiece.position[0]} ${curPiece.position[1]} ${curPiece.position[2]}"
+          rotation="${curPiece.rotation[0]} ${curPiece.rotation[1]} ${curPiece.rotation[2]}"
+          >
+          </a-box>
+          `
+        })
+      })
+  },
+  update: function() {
+  },
+  tick: function() {
   }
 });
 
-
-
-
-
-AFRAME.registerComponent('camera-cube-env', {
+AFRAME.registerComponent('mesh-basic-material', {
+  
   schema: {
-      resolution: { type:'number', default: 128},
-      distance: {type:'number', default: 100000},
-      interval: { type:'number', default: 1000},
-      repeat: { type:'boolean', default: false}
-    },
-    multiple: false,
-
-    init: function(){
-      this.counter = this.data.interval;
-      this.cam = new THREE.CubeCamera( 1.0, this.data.distance, this.data.resolution);
-    
-      this.cam.renderTarget.texture.minFilter = THREE.LinearMipMapLinearFilter;
-      this.cam.renderTarget.texture.generateMipmaps = true;
-      this.el.object3D.add( this.cam );
-
-      this.done = false;
-
-      var myCam = this.cam;
-      var myEl = this.el;
-      var myMesh = this.el.getObject3D('mesh');
-
-      document.querySelector('a-scene').addEventListener('loaded', function (myCam, myEl, myMesh) {
-        if(myMesh){
-          myMesh.traverse( function( child ) { 
-            if ( child instanceof THREE.Mesh ) {
-              child.material.envMap = myCam.renderTarget.texture;
-              child.material.needsUpdate = true;
-            }
-          });
-        }
-      });      
-        
-    },
-    
-    tick: function(t,dt){
-      var myCam = this.cam;
-      if(!this.done){
-        if( this.counter > 0){
-          this.counter -=dt;
-        } else {
-
-          this.mesh = this.el.getObject3D('mesh');
-          
-          if(this.mesh){
-              this.mesh.visible = false;
-
-              AFRAME.scenes[0].renderer.autoClear = true;
-
-              myCam.position.copy(this.el.object3D.worldToLocal(this.el.object3D.getWorldPosition(myCam.position)));
-              myCam.update( AFRAME.scenes[0].renderer, this.el.sceneEl.object3D );
-
-              this.mesh.traverse( function( child ) { 
-                  if ( child instanceof THREE.Mesh ){
-                    child.material.envMap = myCam.renderTarget.texture;
-                    child.material.needsUpdate = true;
-                  }
-              });
-              
-              this.mesh.visible = true;
-          
-              if(!this.data.repeat){
-                this.done = true;
-                this.counter = this.data.interval;
+    map: {default: ""},
+},
+  
+  init: function(){
+      let el = this.el;
+      let mapSrc = this.data.map
+      let texture = new THREE.TextureLoader().load(mapSrc)
+      
+      texture.flipY = false;
+      texture.colorSpace = THREE.SRGBColorSpace
+      
+      el.addEventListener("model-loaded", e =>{
+          el.object3D.traverse(function(node){
+              if (node.isMesh){                           
+                  node.material = new THREE.MeshBasicMaterial({
+                    map: texture
+                  })
+                  node.material.needsUpdate = true;
               }
-          }
-        }
-      }
-    },
-
-    update: function (oldData) {
-      this.counter = this.data.interval;
-      this.cam = new THREE.CubeCamera( 1.0, this.data.distance, this.data.resolution);
-      this.cam.renderTarget.texture.minFilter = THREE.LinearMipMapLinearFilter;
-      this.el.object3D.add( this.cam );
-      this.done = false;
-      var myCam = this.cam;
-        
-      this.mesh = this.el.getObject3D('mesh');
-      if(this.mesh){
-        this.mesh.traverse( function( child ) { 
-          if ( child instanceof THREE.Mesh ) {
-            child.material.envMap = myCam.renderTarget.texture;
-            myCam.renderTarget.texture.generateMipmaps = true;
-            child.material.needsUpdate = true;
-          }
-        });
-      }
-    },
-    remove: function () {},
-    pause: function () { },
-    play: function () { }
-});
+          });
+      });
+  }
+}); 
--- chapter08/aframe_webVR_exhibition/js/main.js
+++ chapter08/aframe_webVR_exhibition/js/main.js
@@ -1,337 +1,37 @@
-const videoEl = document.getElementById("videoEl");
-const video = document.querySelector('#video_mp4');
-video.pause();
-
-
-
 window.addEventListener("load", function () {
   start();
 });
 
+const rig = document.querySelector('#rig');
+const btnFirstFloor = document.querySelector('#btn_first_floor');
+const btnSecondFloor = document.querySelector('#btn_second_floor');
+const closeBtn = document.querySelector('#close_btn');
+const popup = document.querySelector('#popup');
+const popupText = document.querySelector('#popup_text');
+
 async function start() {
 
-  return;
-  
-  if (!window.AnimationEvent) {
-    return;
-  }
-  var anchors = document.getElementsByTagName("a");
+  const menuBtns = document.querySelectorAll('.clickable.menu');
 
-  for (var idx = 0; idx < anchors.length; idx += 1) {
-    if (
-      anchors[idx].hostname !== window.location.hostname ||
-      anchors[idx].pathname === window.location.pathname
-    ) {
-      continue;
-    }
-    anchors[idx].addEventListener("click", function (event) {
-      var fader = document.getElementById("fader"),
-        anchor = event.currentTarget;
+  menuBtns.forEach(function(element) {
+    element.addEventListener('click', function() {
+      popup.setAttribute('visible','true');
+      popupText.setAttribute('value', element.id);
+    })
+  })
 
-      var listener = function () {
-        window.location = anchor.href;
-        fader.removeEventListener("animationend", listener);
-      };
-      fader.addEventListener("animationend", listener);
+  closeBtn.addEventListener('click', function() {
+    popup.setAttribute('visible','false');
+  })
 
-      event.preventDefault();
-      fader.classList.add("fade-in");
-    });
-  }
-  
+  btnFirstFloor.addEventListener('click', function() {
+    rig.setAttribute('position','4.65 0 -0.08');
+    rig.setAttribute('rotation','0 90 0');
+  })
+
+  btnSecondFloor.addEventListener('click', function() {
+    rig.setAttribute('position','-3.786310680989204 3.33 -4.8');
+    rig.setAttribute('rotation','0 180 0');
+  })
+
 }
-
-
-
-
-
-
-// document.getElementById("icon_message").addEventListener("click", function (event) {
-//   event.preventDefault();
-//   camAnimator('#shortcut_mensagem', 4000);
-
-//   document.getElementById('navbarSupportedContent').classList.remove('show');
-
-//   const obj = {
-//     'posY': 0,
-//   }
-
-//   AFRAME.ANIME({
-//     targets: obj,
-//     posY: [videoEl.object3D.position.y, 2],
-//     easing: 'easeInOutSine',
-//     duration: 3000,
-//     delay: 1000,
-//     update() {
-//       videoEl.object3D.position.y = obj.posY;
-//     },
-//     complete(anim) {
-//       video.currentTime = 0; 
-//       video.muted = "";
-//       video.play(); 
-//     },
-//   })
-// });
-
-
-// document.getElementById("icon_albuns").addEventListener("click", function (event) {
-
-//   camAnimator('#shortcut_albuns_1', 3000);
-
-//   document.getElementById('navbarSupportedContent').classList.remove('show');
-
-//   setTimeout(function () {
-
-//       camAnimator('#shortcut_albuns_2', 3000);
-
-//   }, 3000);
-  
-// });
-
-
-// document.getElementById("icon_stories").addEventListener("click", function (event) {
-
-//   camAnimator('#shortcut_stories_1', 3000);
-
-//   document.getElementById('navbarSupportedContent').classList.remove('show');
-
-//   setTimeout(function () {
-
-//       camAnimator('#shortcut_stories_2', 3000);
-
-//   }, 3000);
-  
-// });
-
-
-// document.getElementById("icon_curiosidades").addEventListener("click", function (event) {
-
-//   camAnimator('#shortcut_curiosidades_1', 3000);
-
-//   document.getElementById('navbarSupportedContent').classList.remove('show');
-
-//   setTimeout(function () {
-
-//       camAnimator('#shortcut_curiosidades_2', 3000);
-
-//   }, 3000);
-  
-// });
-
-
-
-
-// document.getElementById("video").addEventListener("click", function (event) {
-
-//   document.getElementById('navbarSupportedContent').classList.remove('show');
-
-//   if (video.paused) 
-//     video.play(); 
-//   else 
-//     video.pause();
-// });
-
-// document.getElementById("closeBtn").addEventListener("click", function (event) {
-//   video.pause();
-
-//   const obj = {
-//     'posY': 0,
-//   }
-
-//   AFRAME.ANIME({
-//     targets: obj,
-//     posY: [videoEl.object3D.position.y, -2.5],
-//     easing: 'easeInOutSine',
-//     duration: 3000,
-//     delay: 1000,
-//     update() {
-//       videoEl.object3D.position.y = obj.posY;
-//     },
-//     complete(anim) {
-//     },
-//   })
-    
-// });
-
-
-// // document.querySelector( ".closeBtn" ).addEventListener("click", function() {
-// //   this.parentElement.classList.remove('visible');
-// // });
-
-// const globalSlickProperties = {
-//     dots: true,
-//     infinite: true,
-//     speed: 500
-//   }
-
-// document.getElementById("album01_content01").addEventListener("click", function (event) {
-//   document.getElementById("album01_content01_popup").classList.add('visible');
-// });
-
-// document.getElementById("album01_content02").addEventListener("click", function (event) {
-//   document.getElementById("album01_content02_popup").classList.add('visible');
-// });
-
-// document.getElementById("album01_content03").addEventListener("click", function (event) {
-//   document.getElementById("album01_content03_popup").classList.add('visible');
-// });
-
-// document.getElementById("album01_content04").addEventListener("click", function (event) {
-//   document.getElementById("album01_content04_popup").classList.add('visible');
-// });
-
-
-
-// // Minhas Historias
-
-// document.getElementById("stories01_btn").addEventListener("click", function (event) {
-//   document.getElementById("stories01_popup").classList.add('visible');
-//   $('#stories01_popup .gallery').slick(globalSlickProperties);
-// });
-
-// document.getElementById("stories02_btn").addEventListener("click", function (event) {
-//   document.getElementById("stories02_popup").classList.add('visible');
-//   $('#stories02_popup .gallery').slick(globalSlickProperties);
-// });
-
-
-
-// // Curiosidades
-
-// document.getElementById("curiosities01_btn").addEventListener("click", function (event) {
-//   document.getElementById("curiosities01_popup").classList.add('visible');
-//   $('#curiosities01_popup .gallery').slick(globalSlickProperties);
-// });
-
-// document.getElementById("curiosities02_btn").addEventListener("click", function (event) {
-//   document.getElementById("curiosities02_popup").classList.add('visible');
-//   $('#curiosities02_popup .gallery').slick(globalSlickProperties);
-// });
-
-
-
-
-
-
-
-// window.addEventListener("pageshow", function (event) {
-//   if (!event.persisted) {
-//     return;
-//   }
-//   var fader = document.getElementById("fader");
-//   fader.classList.remove("fade-in");
-  
-// });
-
-
-
-
-
-
-
-
-    //convert vector source cam position
-    function objToPosS(posObject) {
-
-      let gapX = posObject.x;
-      let gapZ = posObject.z;
-
-      return gapX + " " + posObject.y + " " + gapZ;
-    }
-
-    //convert vector target cam position
-    function objToPosT(posObject) {
-
-      let gapX;
-      let gapZ;
-
-      if (posObject.x >= 0) {
-        gapX = posObject.x - 0
-      } else {
-        gapX = posObject.x + 0
-      }
-
-      if (posObject.z >= 0) {
-        gapZ = posObject.z - 0
-      } else {
-        gapZ = posObject.z + 0
-      }
-
-      return gapX + " " + posObject.y + " " + gapZ;
-    }
-
-    //convert vector target cam rotation 
-    function objToRotT(posObject) {
-      return posObject.x + " " + posObject.y + " " + posObject.z;
-    }
-
-    function objToRotS(posObject) {
-      let adjCam = posObject.y;
-      return posObject.x + " " + adjCam + " " + posObject.z;
-    }
-
-
-    //camera click-animation
-    function camAnimator(target, duration) {
-
-      let cameraEnt = document.querySelector('#camera');
-      let cameraEntRot = objToRotS(cameraEnt.getAttribute("rotation"));
-      let cameraEntLook = document.querySelector('#camera').components['look-controls'];
-      cameraEnt.setAttribute('look-controls', 'enabled', false);
-      cameraEntLook.pitchObject.rotation.x = 0;
-      cameraEntLook.yawObject.rotation.y = 0;
-      cameraEnt.setAttribute('animation', `property: rotation; from: ${cameraEntRot}; to: 0 0 0; dur: ${duration}; easing: easeInOutSine; `);
-
-      let cameraRig = document.querySelector('#rig');
-
-
-      let camPos = objToPosS(cameraRig.getAttribute("position"));
-      let camRot = objToRotS(cameraRig.getAttribute("rotation"));
-
-
-
-      let goal = document.querySelector(target)
-
-      let targetPos = objToPosT(goal.getAttribute("position"));
-      let targetRot = objToRotT(goal.getAttribute("rotation"));
-
-
-      // console.log(":: Source ::" + camPos + ":: " + camRot + ":: Target ::" + targetPos + ":: " + targetRot);
-      // console.log(goal.object3D)
-
-      cameraRig.setAttribute('animation', `property: position; from: ${camPos}; to: ${targetPos}; dur: ${duration}; easing: easeInOutSine; `);
-      cameraRig.setAttribute('animation__2', `property: rotation; from: ${camRot}; to: ${targetRot}; dur: ${duration}; easing: easeInOutSine; `);
-
-      setTimeout(function () {
-
-        cameraEnt.setAttribute('look-controls', 'enabled', true);
-
-      }, 5000);
-
-
-    }
-
-    function videoAutoPlay() {
-
-      let vid = document.querySelector('#video_mp4');
-      /*       if (!vid) {return;}
-            
-            vid.load();
-            vid.play(); */
-      vid.src = "./assets/videos/video_manifest.mp4";
-      vid.autoplay = false;
-      //console.log(vid);
-
-      setTimeout(function () {
-        vid.src = "./assets/videos/video_manifest.mp4";
-        vid.muted = "muted";
-        vid.autoplay = true;
-        vid.pause();
-        vid.play();
-
-
-      }, 5000);
-
-    }
-    // videoAutoPlay();
-
